821dbba7f05e2960de64d8fc665d95811c703c03
Ayman_Rabaya <87930881+AymanRabaya20@users.noreply.github.com>
2024-03-25 22:32:55 +0200

    refactor: Improved Spacing and Indentation.

--- bikeshare.py
+++ bikeshare.py
@@ -36,16 +36,7 @@ def get_filters():
 
 
 def load_data(city, month, day):
-    """
-    Loads data for the specified city and filters by month and day if applicable.
 
-    Args:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-    Returns:
-        df - Pandas DataFrame containing city data filtered by month and day
-    """
     df = pd.read_csv(CITY_DATA[city])
 
     df['Start Time'] = pd.to_datetime(df['Start Time'])
@@ -61,7 +52,7 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
@@ -83,7 +74,7 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
@@ -106,7 +97,7 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
@@ -124,7 +115,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
